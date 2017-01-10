4defa7afce756bf59355539cac858788e3c919d5
jags2-0 <jessica@boomtownroi.com>
2017-01-10 13:49:55 -0500

    Add part1

---
+++ part1.rb
@@ -0,0 +1,16 @@
+# Create an array of the words in the sentence below
+# Find how many words have a length equal to 5
+
+text = "Tacos must not have lettuce Only meat and cheese and salsa"
+words = text.split
+
+puts "#{words} (#{words.class})"
+
+count = 0
+words.each do |word|
+  if word.length == 5
+    count += 1
+  end
+end
+
+puts "#{count} words have length 5"
